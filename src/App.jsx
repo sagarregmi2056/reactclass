a876a0384dbd020c5f90e6ba27226fa9638cb830
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2025-02-17 09:36:56 +0545

    added hero and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 import Landingpage from "./pages/Landingpage";
 
 function App() {
   return (
     <>
+      <Navbar />
+
       <Landingpage />
     </>
   );
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import Button from "../components/Button";
+
 import Card from "../components/Card";
 
 const Landingpage = () => {
   return (
     <div>
-      <Sidebar />
+
+    <Hero/>
 
       <Card title="Card title1111" name="Card name3333" img="./image.png" />
       <Card title="Card title22222" name="Card name222" img="./image2.png" />
-      <Card title="Card title44444" name="Card name1111" />
-      <Card title="Card 5555555" name="Card name6666" />
-      <Card title="Card title 55555343434" name="Card name4444" />
-      <Card title="Card title 3434343" name="Card name77777" />
+      <Card title="Card title44444" name="Card name1111" img="./image.png" />
+      <Card title="Card 5555555" name="Card name6666" img="./image2.png" />
+      <Card
+        title="Card title 55555343434"
+        name="Card name4444"
+        img="./image.png"
+      />
+      <Card
+        title="Card title 3434343"
+        name="Card name77777"
+        img="./image2.png"
+      />
     </div>
   );
 };
